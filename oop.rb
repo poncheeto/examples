68bf7c1e01c05868a0a2814bfc699c6ab3c22cf4
Poncho <jplyles1212@gmail.com>
2020-12-26 15:13:57 -0800

    Practice with using super keyword

--- oop.rb
+++ oop.rb
@@ -1,34 +1,17 @@
 class Base
-  def initialize
-    @name = "Base"
-  end
-
-  private
-
-  def private_method
-    puts "private method called"
-  end
-
-  protected
-
-  def protected_method
-    puts "protected method called"
-  end
-
-  public
-
-  def get_name
-    @name
+  def show x=0, y=0
+    p "Base class, x: #{x}, y: #{y}"
   end
 end
 
 class Derived < Base
-  def public_method
-    private_method
-    protected_method
+  def show x, y
+    super
+    super x
+    super x, y
+    super()
   end
 end
 
 d = Derived.new
-d.public_method
-puts d.get_name
+d.show 3, 3
