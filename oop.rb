8fc9974e1eba35820710fbf428b07006e9b085d9
Poncho <jplyles1212@gmail.com>
2020-12-26 13:48:41 -0800

    Demonstrate two ways to call a method

--- oop.rb
+++ oop.rb
@@ -1,17 +1,14 @@
 class Person
-  def initialize(name="unknown", age = 0)
+  def initialize(name)
     @name = name
-    @age = age
   end
 
-  def to_s
-    "Name: #{@name}, Age: #{@age}"
+  def get_name
+    @name
   end
 end
 
-p1 = Person.new
-p2 = Person.new("unknown", 17)
-p3 = Person.new("Becky", 19)
-p4 = Person.new("Robert")
+per = Person.new("Jane")
 
-puts p1, p2, p3, p4
+puts per.get_name
+puts per.send :get_name
