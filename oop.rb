62429b10d062b251bef1a3196f88d2a450462bbb
Poncho <jplyles1212@gmail.com>
2020-12-27 09:05:37 -0800

    Use + as an overload operator to add two objects together

--- oop.rb
+++ oop.rb
@@ -1,9 +1,21 @@
-class Being
+class Circle
+  attr_accessor :radius
+
+  def initialize r
+    @radius = r
+  end
+
+  def +(other)
+    Circle.new @radius + other.radius
+  end
+
   def to_s
-    "This is Being class"
+    "Circle with radius: #{@radius}"
   end
 end
 
-b = Being.new
-puts b.to_s
-puts b
+c1 = Circle.new(5)
+c2 = Circle.new(6)
+c3 = c1 + c2
+
+puts c3
