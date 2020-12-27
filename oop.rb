b284cd6e9ad66e44448133c6d7db9111b174be69
Poncho <jplyles1212@gmail.com>
2020-12-27 08:48:34 -0800

    Practice with getter and setter methods

--- oop.rb
+++ oop.rb
@@ -1,17 +1,23 @@
-class Base
-  def show x=0, y=0
-    p "Base class, x: #{x}, y: #{y}"
+class Car
+  attr_reader :name, :price
+  attr_writer :name, :price
+  
+  def to_s
+    "#{name}: #{price}"
   end
+
 end
 
-class Derived < Base
-  def show x, y
-    super
-    super x
-    super x, y
-    super()
-  end
-end
+c1 = Car.new
+c2 = Car.new
 
-d = Derived.new
-d.show 3, 3
+c1.name = "Porsche"
+c1.price = 23500
+
+c2.name = "Volkswagen"
+c2.price = 9500
+
+puts "The #{c1.name} costs #{c1.price}"
+
+puts c1
+puts c2
