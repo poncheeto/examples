38baae792abb9931f6685257448af96978da0016
Poncho <jplyles1212@gmail.com>
2020-12-30 13:18:05 -0800

    Add ! after map for it to act on the array it is called on

--- debugging.rb
+++ debugging.rb
@@ -1,6 +1,6 @@
 def double_words_in_phrase(string)
   string_array = string.split(' ')
-  p string_array.map { |word| word * 2 }
+  p string_array.map! { |word| word * 2 }
   string_array.join(' ')
 end
 
